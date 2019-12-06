cf406bdd35e4bd06b29b4a1e0737e057bb6aea2c
Tal Litvak <litvaktal04@gmail.com>
2019-12-07 00:59:29 +0200

    practicing mongoose schema

--- index.js
+++ index.js
@@ -1,20 +1,39 @@
-const express = require("express");
-const app = express();
+const express = require("express"),
+  app = express(),
+  url = process.env.MONGO_URI || "mongodb://localhost:27017/countme",
+  mongoose = require("mongoose"),
+  db = mongoose.connection,
+  Schema = mongoose.Schema,
+  PORT = process.env.PORT || 5000;
 
-const MONGO = process.env.MONGO_URI || "mongodb://localhost:27017/restapi";
-const mongoose = require("mongoose");
-mongoose.connect(MONGO, {
+mongoose.connect(url, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
 
-const db = mongoose.connection;
-db.on("error", () => console.log("error connection"));
+db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", () => console.log("CONNECTED!"));
 
-app.get("/hello", (req, res) => res.send("Hello Bro!!!"));
+const weddingSchema = new mongoose.Schema({
+  name: String
+});
 
+const Wedding = mongoose.model("Wedding", weddingSchema);
+
+const ganHapekan = new Wedding({ name: "ganHapekan" });
+
+ganHapekan.save(function(err, wedding) {
+  if (err) return console.error(err);
+  console.log(`SAVED: ${wedding.name}`);
+});
+
+Wedding.find((err, wed) => {
+  if (err) return console.error(err);
+  console.log(wed);
+});
+
+app.get("/", (req, res) => res.send("Home page"));
+app.get("/hello", (req, res) => res.send("Hello Bro!!!"));
 app.use(express.json());
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`server is on ${PORT} `));
+app.listen(PORT, () => console.log(`server is on port ${PORT} `));
